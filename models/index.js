c28e9e63db01f6ff489651923a76b4395457d2bc
jino <jho378@naver.com>
2021-12-17 16:05:22 +0900

    [feature] index.js 작성

--- models/index.js
+++ models/index.js
@@ -7,8 +7,8 @@ const Map = require('./Map');
 const Monster = require('./Monster');
 const User = require('./User');
 
-const mongoURL = 'mongodb://venture:dating@cluster0-shard-00-00.cpqk4.mongodb.net:27017,cluster0-shard-00-01.cpqk4.mongodb.net:27017,cluster0-shard-00-02.cpqk4.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-8n3khm-shard-0&authSource=admin&retryWrites=true&w=majority';
-//' mongodb://venture:<password>@cluster0-shard-00-00.cpqk4.mongodb.net:27017,cluster0-shard-00-01.cpqk4.mongodb.net:27017,cluster0-shard-00-02.cpqk4.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-8n3khm-shard-0&authSource=admin&retryWrites=true&w=majority'
+const mongoURL = 'mongodb://venture:<password>@cluster0-shard-00-00.cpqk4.mongodb.net:27017,cluster0-shard-00-01.cpqk4.mongodb.net:27017,cluster0-shard-00-02.cpqk4.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-8n3khm-shard-0&authSource=admin&retryWrites=true&w=majority';
+
 mongoose.connect(mongoURL);
 if(mongoose.connect(mongoURL))  console.log('connected to DB')
 
