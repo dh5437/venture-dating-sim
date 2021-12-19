bc04fd59350194b9a3cff32bc11e06d1b5cd03b2
yeji <lerry597@gmail.com>
2021-12-19 11:10:52 +0900

    [feature] rest.js 작성

--- routes/rest.js
+++ routes/rest.js
@@ -5,7 +5,7 @@ const { setAuth } = require('../utils');
 
 router.get('/', setAuth, (req, res) => {
   const user = req.user;
-  const log = '도망쳤습니다!';
+  const log = '아무 일도 일어나지 않았다.';
 
   res.send({ user, log });
 });
