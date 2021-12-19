b938c2cade1541c5087ee60afb4a3878ba9f92d1
yeji <lerry597@gmail.com>
2021-12-19 11:17:07 +0900

    [chore] item.js 흐름 주석 정리

--- routes/item.js
+++ routes/item.js
@@ -6,9 +6,13 @@ const { Item } = require('../models');
 
 router.get('/:itemId', setAuth, async (req, res) => {
   const user = req.user;
-  // 아이템 받아오기
   const { itemId } = req.params;
+
+  // 아이템 받아오기
   // log: 아이템 { }을 획득했다! { }가 { } 상승했다!
+  // 아이템에 따른 str과 def, hp 변화 반영
+  // user의 items에 업데이트 -> client에 쏴주기
+  // client에 뿌려줄 것들: user 모델(items , 변경된 유저 정보), 획득한 아이템 이름, 메시지(로그)
 
   await item.save();
   await user.save();
