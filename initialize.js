f5d1643e0d4ef2405a0638286fdc9ad68a914bd6
jino <jho378@naver.com>
2021-12-22 03:52:30 +0900

    [feature] initialize.js 작성

--- initialize.js
+++ initialize.js
@@ -61,84 +61,94 @@ const init = async () => {
   fs.writeFileSync('./datas/monsters.json', JSON.stringify(monsterJson));
 
   const items = [
-    '러브 레터',
-    '꽃다발',
-    '향수',
-    '참이슬',
-    '코트',
-    '투명 뿔테 안경',
-    '트레이닝 바지',
-    '현금',
-    '비트코인',
-    '이더리움',
-    '통장',
-    '따뜻한 스웨터',
-    '목도리',
-    '현란한 카톡멘트',
+    ['러브 레터', '그녀에게 마음을 전하고 싶다..'],
+    ['꽃다발', '그녀보다 예쁘진 않은 걸..'],
+    ['향수', '샤넬 넘버 5.. 그녀가 좋아할까?'],
+    ['참이슬', '그래.. 나 취했는지도 몰라.. 실수인지도 몰라..'],
+    ['코트', '이걸 입으면 멋진 남자가 될 수 있을까..?'],
+    ['투명 뿔테 안경', '박보검이 썼을 땐 멋지던데..'],
+    ['트레이닝 바지', '3대 500, 가뿐하겠지..?'],
+    ['현금', '이걸로 그녀를 위한 선물을 살 수 있겠지..?'],
+    ['비트코인', '제발 올라줘..'],
+    ['이더리움', '떨어지지만 말아줘..'],
+    ['통장', '그녀를 위해서라면 텅장이 되어도 좋아..'],
+    ['따뜻한 스웨터', '감기 걸리면 안되니까..'],
+    ['목도리', '배용준이 둘렀을 땐 멋지던데..'],
+    ['현란한 카톡멘트', '이걸로 그녀의 환심을 사고파..'],
   ];
+
   const itemJson = [];
-  for (const _item of items) {
+  items.forEach(async (_item, index) => {
     const property = ['hp', 'exp', 'str'];
     const value = property[Math.floor(Math.random() * 3)];
     if (value === 'hp') {
       const randomhp = Math.ceil(Math.random() * 50);
       const item = new Item({
-        name: _item,
+        name: _item[0],
         hp: randomhp,
         exp: 0,
         str: 0,
-        id: items.indexOf(_item),
+        id: index,
+        description: _item[1],
         isActive: true,
       });
       itemJson.push({
-        name: _item,
+        name: _item[0],
         hp: randomhp,
         exp: 0,
         str: 0,
-        id: items.indexOf(_item),
+        id: index,
+        description: _item[1],
         isActive: true,
       });
       await item.save();
     } else if (value === 'exp') {
       const randomexp = Math.ceil(Math.random() * 40);
       const item = new Item({
-        name: _item,
+        name: _item[0],
         hp: 0,
         exp: randomexp,
         str: 0,
-        id: items.indexOf(_item),
+        id: index,
+        description: _item[1],
         isActive: true,
       });
       itemJson.push({
-        name: _item,
+        name: _item[0],
         hp: 0,
         exp: randomexp,
         str: 0,
-        id: items.indexOf(_item),
+        id: index,
+        description: _item[1],
         isActive: true,
       });
       await item.save();
     } else {
       const randomstr = Math.ceil(Math.random() * 20);
       const item = new Item({
-        name: _item,
+        name: _item[0],
         hp: 0,
         exp: 0,
         str: randomstr,
-        id: items.indexOf(_item),
+        id: index,
+        description: _item[1],
         isActive: true,
       });
       itemJson.push({
-        name: _item,
+        name: _item[0],
         hp: 0,
         exp: 0,
         str: randomstr,
-        id: items.indexOf(_item),
+        id: index,
+        description: _item[1],
         isActive: true,
       });
       await item.save();
     }
-  }
+  });
+  // for (const _item of items) {
+
+  // }
   fs.writeFileSync('./datas/items.json', JSON.stringify(itemJson));
 
   const coordinates = [];
