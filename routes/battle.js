e2c14cb61c12fa781768a330d255fbbc5abeb344
김Kim Do hyun <dh5437@naver.com>
2021-12-21 23:25:58 +0900

    [fix] battle.js 수정

--- routes/battle.js
+++ routes/battle.js
@@ -5,43 +5,65 @@ const { setAuth } = require('../utils');
 const { Monster } = require('../models');
 
 router.get('/:turn', setAuth, async (req, res) => {
-    const user = req.user;
-    const { turn } = req.params;
-    const monster = await Monster.findOne({ name });
-    const userItems = user.items;
-    const log = `적 ${monster.name}을 만났다.`;
+  const user = req.user;
+  const turn = +req.params.turn;
+  const { name } = req.params;
+  const monster = await Monster.findOne({ name });
+  const userItems = user.items;
 
-    if (turn <0 ) {
-        return res.sendStatus(404)
-    } else if (turn === 0) {
-        return res.status(200).send( { user, monster, log } )
-    } else if (turn>10 || user.hp/user.maxhp<=0.2) {
-        return res.status(200).send('도망간다/도망가지 않는다.')
+  if (turn < 1) {
+    return res.sendStatus(404);
+  } else if (turn > 10 || user.hp / user.maxhp <= 0.2) {
+    const isEnded = false;
+    const canEscape = true;
+    return res.status(200).send({ canEscape, isEnded });
+  } else {
+    if (monster.hp <= 0) {
+      user.hp = user.maxhp;
+      user.exp += monster.exp;
+      if (user.exp >= 100) {
+        user.level += 1;
+        user.exp -= 100;
+      }
+      await user.save();
+      const message = `${monster.name}을 무찔렀습니다! 경험치가 ${monster.exp}만큼 회복되었습니다!
+        "${monster.name}... 별 거 아니군.."`;
+      const isEnded = false;
+      const canEscape = false;
+      return res.status(200).send({ user, message, isEnded, canEscape });
+    } else if (user.hp <= 0) {
+      userItems.forEach((e) => {
+        if (Math.random() < 0.5) {
+          e.delete;
+        }
+      });
+      user.hp = user.maxhp * 0.7;
+      await user.save();
+      const message = `${monster.name}에게 당했습니다. 처음으로 돌아갑니다.
+        "그녀의 ${monster.name}은 강하구나.. 조심해야지.."`;
+      const isEnded = true;
+      const canEscape = false;
+      return res.status(200).send({ user, message, isEnded, canEscape });
     } else {
-    if (monster.hp<=0) {
-        user.hp = user.maxhp
-        user.exp += 10
-        await user.save()
-        const winLog = '적을 물리쳤습니다.'
-        return res.status(200).send( { user, winLog } )
-    } else if (user.hp<=0) {
-        user.map.coordinate = ( 0,0 )
-        userItems.forEach(e => {if (Math.random()<0.5) {e.delete}} )
-        await user.save()
-        const isEnded = '적에게 당했습니다.'
-        return res.status(200).send( { user, isEnded } )
-    } else {
-    if (Math.random()<0.7) {
+      if (Math.random() < 0.7) {
         monster.hp -= user.str - monster.def;
         await monster.save();
-        const log2 = '공격이 성공하였습니다.';
-        return res.status(200).send( { user, monster, log2 } );
-    } else {
+        const message = `그녀의 ${monster.name}에게 상처를 입혔다! 통쾌하다.
+        ${user.str - monster.def}의 피해를 입혔다! (적의 남은 체력 : ${monster.hp})`;
+        const isEnded = false;
+        const canEscape = false;
+        return res.status(200).send({ user, monster, message, isEnded, canEscape });
+      } else {
         user.hp -= monster.str - user.def;
         await user.save();
-        const log2 = '적이 공격하였습니다.';
-        return res.status(200).send( { user, monster, log2 } );
-    }}}
+        const message = `그녀의 ${monster.name}이 공격에 성공했다! 아프다!
+        ${monster.str - user.def}의 피해를 입었다! (적의 남은 체력 : ${monster.hp})`;
+        const isEnded = false;
+        const canEscape = false;
+        return res.status(200).send({ user, monster, message, isEnded, canEscape });
+      }
+    }
+  }
 });
 
 module.exports = router;
